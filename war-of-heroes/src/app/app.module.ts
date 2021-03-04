bb6f80f1aa64120d3b9b54773d9f27ed6582d753
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-03-04 16:24:27 +0000

    add-auth

---
+++ war-of-heroes/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    SocialLoginModule
+  ],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('')
+          }
+        ]
+      } as SocialAuthServiceConfig
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
