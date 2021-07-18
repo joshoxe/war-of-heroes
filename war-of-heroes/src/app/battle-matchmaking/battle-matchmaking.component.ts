9ef5f784f2f9e830f8e8f479430f19b56ceef8dd
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-07-18 15:00:22 +0100

    Finish matchmaking and add rudimentary game
    * (Very) MVP of a game - first to click wins

--- war-of-heroes/src/app/battle-matchmaking/battle-matchmaking.component.ts
+++ war-of-heroes/src/app/battle-matchmaking/battle-matchmaking.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { io }from 'socket.io-client';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-battle-matchmaking',
@@ -10,32 +10,60 @@ import { io }from 'socket.io-client';
 export class BattleMatchmakingComponent implements OnInit {
   private socket;
   public gameReady: boolean = false;
+  public matchmakingInProgress: boolean = false;
+  public gameOver: boolean = false;
   public text: string;
+  public winner: boolean;
+  public image: string = "";
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
-  connect(): any {
+  connect(): void {
+    this.gameOver = false;
+    this.gameReady = false;
+    this.matchmakingInProgress = true;
     this.socket = io("http://localhost:3000");
 
     this.socket.onAny((eventName, data) => {
-      console.log("Received event: ", eventName);
 
       if (eventName == "matchmaking") {
-        console.log("Event was a matchmaking request, sending name");
-        this.socket.emit("matchmaking", ["TestName", [1,2,3,4]]);
+        const name = this.userService.getUserFirstName();
+        this.userService.getUserDeck().subscribe((ids) => {
+          // Pass the auth tokens so the server can call API endpoints for user data
+          this.socket.emit("matchmaking", [name, this.userService.getUserJwtToken(), this.userService.getUserAccessToken()]);
+        });
       }
 
       if (eventName == "ready") {
         this.gameReady = true;
-        this.text = data;
+        this.matchmakingInProgress = false;
       }
 
       if (eventName == "message"){
         console.log(data);
       }
+
+      if (eventName == "win") {
+        this.image = "assets/images/youwin.gif"
+        this.winner = true;
+        this.gameOver = true;
+        this.gameReady = false;
+      }
+
+      if (eventName == "lose") {
+        this.image = "assets/images/youlose.gif"
+        this.winner = false;
+        this.gameOver = true;
+        this.gameReady = false;
+      }
     })
+  }
+
+  winGame() {
+    console.log("Hit the win button!");
+    this.socket.emit("win");
   }
 }
