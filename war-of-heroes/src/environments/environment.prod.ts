44162b9094ff29c67116e3cd4afecb7322f6ae74
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-07-24 14:05:14 +0100

    Fix prod URL

--- war-of-heroes/src/environments/environment.prod.ts
+++ war-of-heroes/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   userApiUrl : 'https://warofheroesusers.azurewebsites.net',
   heroesApiUrl: 'https://warofheroesheroes.azurewebsites.net/hero',
-  gameServerUrl: 'https://warofheroesserver.azurewebsites.net:3000/' // TODO: Fill this when known
+  gameServerUrl: 'https://warofheroesserver.azurewebsites.net/'
 };
