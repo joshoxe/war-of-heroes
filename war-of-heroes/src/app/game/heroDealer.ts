e2ce738b8544f89b50a144e2393f6d6d2274419a
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-08-17 16:01:45 +0100

    Dynamically deal cards for both users

--- war-of-heroes/src/app/game/heroDealer.ts
+++ war-of-heroes/src/app/game/heroDealer.ts
@@ -1,4 +1,5 @@
 import { Scene } from 'phaser';
+import { BattleGameComponent } from '../battle-game/battle-game.component';
 import { Hero } from '../hero';
 import Card from './card';
 
@@ -14,14 +15,19 @@ export class HeroDealer {
     }
 
     loadCards() {
+        var width = BattleGameComponent.WIDTH;
+        var height = BattleGameComponent.HEIGHT;
+
+        var cardStartingPoint = width * 0.1;
+        var userCardVerticalPoint = height * 0.38;
         for (let i = 0; i < this.userDeck.length; i++) {
             const hero = this.userDeck[i];
-            new Card(this.scene, hero, 475 + i * 160, 700);
+            new Card(this.scene, hero, cardStartingPoint + i * 110, userCardVerticalPoint);
           }
 
         for (let i = 0; i < this.opponentDeck.length; i++) {
             const hero = this.opponentDeck[i];
-            new Card(this.scene, hero, 475 + i * 160, 200, false);
+            new Card(this.scene, hero, cardStartingPoint + i * 110, 90, false);
         }
     }
 }
