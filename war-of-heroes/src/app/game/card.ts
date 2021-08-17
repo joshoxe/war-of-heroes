f2c3a8bb100faa4823993ec1132862c9b78e93ce
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-08-17 16:00:50 +0100

    Fix card container
    - Display card and text (placeholder)
    - Make container draggable
    - Store hero data in container

--- war-of-heroes/src/app/game/card.ts
+++ war-of-heroes/src/app/game/card.ts
@@ -4,14 +4,13 @@ import { Hero } from "../hero";
 export default class Card {
     constructor(scene: Scene, hero: Hero, x: number, y: number, draggable: boolean = true) {
         var card = scene.add.image(x, y, 'heroCard').setScale(0.3, 0.3)
-        //var heroName = scene.add.text(0, 0, hero.name, {font: "16px Arial", color: "#333333"});
-        //var cardContainer = scene.add.container(x, y, [heroName, card]).setInteractive(new Phaser.Geom.Circle(0, 0, 100), Phaser.Geom.Circle.Contains);
+        var heroName = scene.add.text(x - 60, y + 50, hero.name, {font: "14px Arial", color: "#333333", });
+        var cardContainer = scene.add.container(x, y, [card, heroName]).setInteractive(new Phaser.Geom.Circle(x, y, 100), Phaser.Geom.Circle.Contains);
+        cardContainer.setData({hero: hero});
 
         if (draggable) {
-            card.setInteractive();
-            scene.input.setDraggable(card);
+            cardContainer.setInteractive();
+            scene.input.setDraggable(cardContainer);
         }
-
-        // TODO: Replace placeholder card image, add text from hero object and place it into a draggable container
     }
 }
