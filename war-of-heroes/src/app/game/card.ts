498c954ae52d43ce0d251ec09939c548c711e04c
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-08-18 17:31:21 +0100

    Add card draw animation

--- war-of-heroes/src/app/game/card.ts
+++ war-of-heroes/src/app/game/card.ts
@@ -5,8 +5,22 @@ export default class Card {
     constructor(scene: Scene, hero: Hero, x: number, y: number, draggable: boolean = true) {
         var card = scene.add.image(x, y, 'heroCard').setScale(0.3, 0.3)
         var heroName = scene.add.text(x - 60, y + 50, hero.name, {font: "14px Lato", color: "#333333", });
-        var cardContainer = scene.add.container(x, y, [card, heroName]).setInteractive(new Phaser.Geom.Circle(x, y, 100), Phaser.Geom.Circle.Contains);
+        var cardContainer = scene.add.container(x - 100, y, [card, heroName]).setInteractive(new Phaser.Geom.Circle(x, y, 100), Phaser.Geom.Circle.Contains);
         cardContainer.setData({hero: hero});
+        cardContainer.setAlpha(0);
+        scene.tweens.add({
+            targets: cardContainer,
+            alpha: 1,
+            duration: 2000,
+            ease: 'Power2'
+        });
+
+        scene.tweens.add({
+            targets: cardContainer,
+            x: x,
+            duration: 500,
+            ease: 'Power2'
+        });
 
         if (draggable) {
             cardContainer.setInteractive();
